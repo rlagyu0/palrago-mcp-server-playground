2146aad483b336db7e0cb081d4c1f504c4229416
DESKTOP-36NSF72\KGY <01086055223@hanmail.net>
2025-09-12 00:40:33 +0900

    add: logger

--- mcp-client/src/main/kotlin/mcp/client/controller/BasicController.java
+++ mcp-client/src/main/kotlin/mcp/client/controller/BasicController.java
@@ -1,6 +1,7 @@
 package mcp.client.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.tool.ToolCallbackProvider;
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 public class BasicController {
     private final ChatClient chatClient;
 
     public BasicController(ChatClient.Builder clientBuilder, ToolCallbackProvider tools) {
         this.chatClient = clientBuilder
-                .defaultSystem("Please priorities context information for answering queries. Give short, concide and to the point answers.")
+                .defaultSystem("Return the value from the tool exactly as it is, without modifying or interpreting it in any way.")
                 .defaultToolCallbacks(tools)
                 .build();
     }
@@ -22,18 +25,15 @@ public class BasicController {
     @GetMapping("/chat")
     public String chat(@RequestParam String query) {
 
-        //        val response = chatClient.complete(
-        //                ChatCompletionRequest.builder()
-        //                        .model("gpt-4")
-        //                        .messages(listOf(ChatMessage.ofUser(question)))
-        //                        .build()
-        //        )
-        //        return response.firstChoice().message().content()
-
         PromptTemplate promptTemplate = new PromptTemplate(query);
         Prompt prompt = promptTemplate.create();
-        ChatClient.CallResponseSpec res = chatClient.prompt(prompt).call();
-
+        ChatClient.CallResponseSpec res = chatClient.prompt(prompt)
+                .advisors(new SimpleLoggerAdvisor(
+                        request -> String.format("[advisor] request : %s", request.prompt().toString()),
+                        response -> String.format("[advisor] response : %s", Arrays.toString(response.getResults().toArray())),
+                        0
+                ))
+                .call();
         return res.content();
     }
 }
--- mcp-server/src/main/kotlin/mcp/server/tools/PalragoService.kt
+++ mcp-server/src/main/kotlin/mcp/server/tools/PalragoService.kt
@@ -1,18 +1,33 @@
 package mcp.server.tools
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.ai.tool.annotation.Tool
+import org.springframework.ai.tool.annotation.ToolParam
 import org.springframework.stereotype.Service
 
 @Service
 class PalragoService {
 
     @Tool(name = "search_market_product",
-        description = "search for products in the marketplace")
-    fun searchMarketProduct(productName: String, brandName: String): String {
+        description = "search for products in the marketplace",
+        returnDirect = true)
+    fun searchMarketProduct(
+                                    @ToolParam(description = "search product name") productName: String,
+                                    @ToolParam(description = "search brand name of search product") brandName: String
+                            ): Response {
 
-        val safeProductName = productName?.takeIf { it.isNotBlank() } ?: "상품 테스트"
-        val safeBrandName = brandName?.takeIf { it.isNotBlank() } ?: "브랜드 테스트"
+        val safeProductName = productName?.takeIf { it.isNotBlank() } ?: "상품명 미입력"
+        val safeBrandName = brandName?.takeIf { it.isNotBlank() } ?: "브랜드명 미입력"
 
-        return "상품명은 [$safeProductName] 이며, 브랜드 이름은 [$safeBrandName] 입니다."
+//        val product = Response(safeProductName, safeBrandName)
+
+//        val json = jacksonObjectMapper().writeValueAsString(product)
+
+        return Response(safeProductName, safeBrandName)
     }
+
+    data class Response(
+        val productName: String?,
+       val brandName: String?
+    )
 }
